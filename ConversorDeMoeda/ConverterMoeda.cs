7ba190ce04758fb1af856980c565db8c085c0fc6
gabb-z <gabbz.liima@gmail.com>
2023-03-22 22:53:16 -0300

    v1 - Projeto ConversorDeMoeda

---
+++ ConversorDeMoeda/ConverterMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class ConverterMoeda
+    {
+        //Valor Estático
+        public static double Iof = 6.0;
+
+        //Função de Conversão - Estática
+        public static double DolarParaReal(double cotacao, double quantidade)
+        {
+            double resultado = cotacao * quantidade;
+            return resultado += Iof * cotacao * quantidade / 100.0;
+        }
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis
+            double cotacaoDolar, qtdeDolar, resultado;
+
+            //Entrada de Dados - Dólar
+            Console.Write("Digite a cotação atual do dólar: ");
+            cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a quantidade de dólar que irá comprar: ");
+            qtdeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Saída de Dados - Resultado
+            resultado = ConverterMoeda.DolarParaReal(cotacaoDolar, qtdeDolar);
+            Console.WriteLine("Valor a ser pago em reais: R$ " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
